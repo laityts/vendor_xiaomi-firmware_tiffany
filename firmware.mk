0aa051f716080f6dc36159ae479409d7de1bcb91
laityts <laityts@gmail.com>
2022-03-28 01:53:35 +0800

    initial commit

---
+++ firmware.mk
@@ -0,0 +1,13 @@
+PRODUCT_COPY_FILES += \
+        vendor/xiaomi/tiffany-firmware/adspso.bin:install/firmware-update/adspso.bin \
+        vendor/xiaomi/tiffany-firmware/cmnlib.mbn:install/firmware-update/cmnlib.mbn \
+        vendor/xiaomi/tiffany-firmware/cmnlib64.mbn:install/firmware-update/cmnlib64.mbn \
+        vendor/xiaomi/tiffany-firmware/devcfg.mbn:install/firmware-update/devcfg.mbn \
+        vendor/xiaomi/tiffany-firmware/emmc_appsboot.mbn:install/firmware-update/emmc_appsboot.mbn \
+        vendor/xiaomi/tiffany-firmware/keymaster.mbn:install/firmware-update/keymaster.mbn \
+        vendor/xiaomi/tiffany-firmware/lksecapp.mbn:install/firmware-update/lksecapp.mbn \
+        vendor/xiaomi/tiffany-firmware/mdtp.img:install/firmware-update/mdtp.img \
+        vendor/xiaomi/tiffany-firmware/NON-HLOS.bin:install/firmware-update/NON-HLOS.bin \
+        vendor/xiaomi/tiffany-firmware/rpm.mbn:install/firmware-update/rpm.mbn \
+        vendor/xiaomi/tiffany-firmware/sbl1.mbn:install/firmware-update/sbl1.mbn \
+        vendor/xiaomi/tiffany-firmware/tz.mbn:install/firmware-update/tz.mbn
